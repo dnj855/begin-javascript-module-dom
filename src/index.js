c01ba98a2e75a08a81f3329a4b269bbe8e56bdf0
Cédric Lang-Roth <contact@clangroth.fr>
2024-04-27 16:41:51 +0200

    End of Part 1

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-class Game {
+class Board {
   static COLORS = ['#ff4500', '#00cc78', '#2450a5', '#fed734', '#f9fafc'];
   static BOARD_SIZE = [25, 25];
   static PIXEL_SIZE = 20;
   static TIME_TO_WAIT = 5000;
 
   constructor() {
-    this.colorPicker = new ColorPicker(Game.COLORS, Game.COLORS[0]);
-    this.board = undefined;
+    this.colorPicker = new ColorPicker(Board.COLORS, Board.COLORS[0]);
   }
 
   init() {
     this.board = document.querySelector('#board');
-    this.board.style.gridTemplateColumns = `repeat(${Game.BOARD_SIZE[0]}, ${Game.PIXEL_SIZE}px)`;
+    this.board.style.gridTemplateColumns = `repeat(${Board.BOARD_SIZE[0]}, ${Board.PIXEL_SIZE}px)`;
     this.initPixels();
-    this.colorPicker.initPixelPicker();
+    this.colorPicker.init();
   }
 
   initPixels() {
-    for (let i = 0; i < Game.BOARD_SIZE[0] * Game.BOARD_SIZE[1]; i++) {
-      const pixel = new Pixel(Game.COLORS[Game.COLORS.length - 1]);
+    for (let i = 0; i < Board.BOARD_SIZE[0] * Board.BOARD_SIZE[1]; i++) {
+      const pixel = new Pixel(Board.COLORS[Board.COLORS.length - 1]);
       this.board.append(pixel.element);
     }
   }
@@ -29,9 +28,9 @@ class Pixel {
   static PIXEL_PICKER_CLASS = 'pixel-picker';
 
   constructor(color) {
-    const _color = color;
+    this._color = color;
     this.element = document.createElement('div');
-    this.element.style.backgroundColor = _color;
+    this.element.style.background = color;
     this.element.classList.add(Pixel.PIXEL_CLASS);
   }
 }
@@ -43,20 +42,20 @@ class ColorPicker {
     this.pixels = [];
   }
 
-  initPixelPicker() {
-    const element = document.querySelector('#color-picker');
+  init() {
+    this.element = document.querySelector('#color-picker');
 
-    for (const color of Game.COLORS) {
+    for (const color of this.colors) {
       const pixel = new Pixel(color);
+      this.pixels.push(pixel);
       pixel.element.classList.add(Pixel.PIXEL_PICKER_CLASS);
       if (color === this.currentColor) {
         pixel.element.classList.add('active');
       }
-      this.pixels.push(pixel);
-      element.append(pixel.element);
+      this.element.appendChild(pixel.element);
     }
   }
 }
 
-const game = new Game();
+const game = new Board();
 game.init();
